a79f4848aea94791a833f4817eac1b2dcc32c4f9
Ieon <yieon1028@gmail.com>
2024-10-01 18:01:39 +0900

    randomcolor함수 추가

--- Project1/Project1-1/Background-color-change.cpp
+++ Project1/Project1-1/Background-color-change.cpp
@@ -1,19 +1,27 @@
 #include <iostream>
 #include <gl/glew.h>
+#include <cstdlib>
+#include <ctime>
 #include <gl/freeglut.h>
 #include <gl/freeglut_ext.h>
 
 GLvoid drawScene(GLvoid);
 GLvoid Reshape(int w, int h);
 GLvoid Keyboard(unsigned char key, int x, int y);
+void RandomColor();
+
+float r = 0.0f; float g = 0.0f; float b = 0.0f;
 
 void main(int argc, char** argv)
-{	//--- 윈도우 생성하기
+{	
+	srand(static_cast<unsigned int>(time(NULL)));
+
+	//--- 윈도우 생성하기
 	glutInit(&argc, argv);							// glut 초기화
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);	// 디스플레이 모드 설정
 	glutInitWindowPosition(620, 250);				// 윈도우의 위치 지정
 	glutInitWindowSize(500, 500);					// 윈도우의 크기 지정
-	glutCreateWindow("Color Change");					// 윈도우 생성(윈도우 이름)
+	glutCreateWindow("Color Change");				// 윈도우 생성(윈도우 이름)
 
 	//--- GLEW 초기화하기
 	glewExperimental = GL_TRUE;
@@ -72,7 +80,14 @@ GLvoid Keyboard(unsigned char key, int x, int y)
 		break;
 	
 	case 'q':
-		break;
+		exit(0);
 	}
+
 	glutPostRedisplay(); //--- 배경색이 바뀔 때마다 출력 콜백 함수를 호출하여 화면을 refresh 한다
+}
+
+void RandomColor() {
+	r = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
+	g = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
+	b = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
 }
